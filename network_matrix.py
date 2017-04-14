13bed8046a014d20adc5d2fdca69b7f2e06e827e
williammileslew <williammileslew@gmail.com>
2017-04-14 00:40:25 -0400

    implemented different activation function feature

--- network_matrix.py
+++ network_matrix.py
@@ -12,6 +12,12 @@ def __logsig(value, derivative = 0):
         return value * (1 - value)
     
     return 1 / (1 + np.exp(-value))
+ 
+def __purelin(value, derivative = 0):
+    if derivative == 1:
+        return 1
+    
+    return value
     
 def generate_array(array, output = 0):
     if output == 1:
@@ -24,20 +30,33 @@ def generate_synapse(array):
         temp_list.append(2*np.random.random((array[x - 1], array[x])) - 1)
     
     return temp_list
+
+def generate_activation(array):
+    temp_list = []
+    for x in array:
+        if x == 0:
+            temp_list.append(__purelin)
+        if x == 1:
+            temp_list.append(__logsig)
     
-def feedforward_and_backpropagate(loop_count, synapse_list, input_case, output_case):
+    return temp_list
+    
+def feedforward_and_backpropagate(loop_count, synapse_list, input_case, output_case, activation_list = None):
+    if activation_list == None:
+        activation_list = [__logsig for x in range(len(synapse_list))]
+        
     for j in xrange(loop_count):
         #feedforward
         layer_list = [input_case]
         for x in range(len(synapse_list)):
-            layer_list.append(__logsig(np.dot(layer_list[x], synapse_list[x])))
+            layer_list.append(activation_list[x](np.dot(layer_list[x], synapse_list[x])))
         
         #backprop
-        layer_delta_list = [(output_case - layer_list[len(layer_list) - 1]) * __logsig(layer_list[len(layer_list) - 1], 1)]
+        layer_delta_list = [(output_case - layer_list[len(layer_list) - 1]) * activation_list[len(activation_list) - 1](layer_list[len(layer_list) - 1], 1)]
         for x in range(len(synapse_list) - 2, -1, -1):
-            layer_delta_list.append(layer_delta_list[len(layer_delta_list) - 1].dot(synapse_list[x + 1].T) * __logsig(layer_list[x + 1], 1))
+            layer_delta_list.append(layer_delta_list[len(layer_delta_list) - 1].dot(synapse_list[x + 1].T) * activation_list[x](layer_list[x + 1], 1))
         
         for x in range(len(synapse_list)):
             synapse_list[x] += layer_list[x].T.dot(layer_delta_list[len(synapse_list) - x - 1])
-        
+            
     return layer_list[len(layer_list) - 1]
